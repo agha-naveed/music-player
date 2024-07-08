f1050f64049dd2dbbad9c4b489363792c60e53e4
Agha Naveed <naveedabs31@gmail.com>
2024-07-08 18:29:14 +0500

    Initial Commit

---
+++ linkedlist.js
@@ -0,0 +1,361 @@
+//  ------------------------ Stack --------------------------
+class Stack {
+    arr = []
+    size;
+    push(value) {
+        this.size = this.arr.length
+        this.arr[this.size] = value   
+    }
+
+    pop() {
+            this.arr.length--
+    }
+
+    display() {
+        return this.arr
+    }
+}
+
+// ---------------------- Stack Ended -----------------------
+// ----------------------------------------------------------
+// ------------------ Doubly Linked List --------------------
+class List {
+    constructor() {
+        this.head = {
+            prev: null,
+            data: null,
+            next: null
+        }
+        this.tail = this.head
+        this.size = 0
+    }
+
+    appendNode(Node) {
+        let node = {
+            prev: this.tail,
+            data: Node,
+            next: null
+        };
+        
+
+        this.tail.next = node;
+        this.tail = node
+        this.size++
+    }
+
+
+    display() {
+        return this.head
+    }
+
+    size(){
+        return this.size
+    }
+}
+// ------------------ Linked List Ended ---------------------
+// ----------------------------------------------------------
+// ---------------------- Array Ended -----------------------
+let playlist = [
+    {
+        name: 'BG Music',
+        item: new List(),
+    },
+    {
+        name: 'Manqabat',
+        item: new List()
+    },
+    {
+        name: 'Qaseeda',
+        item: new List()
+    },
+]
+
+// ---------------------- Array Ended -----------------------
+// ----------------------------------------------------------
+// ------------------------ Audios --------------------------
+
+// Background Music
+playlist[0].item.appendNode({
+    title: 'NIVIRO  Without You feat Justin J Moore  Happy Hardcore  NCS  Copyright Free Music',
+    displayTitle: 'NIVIRO Without You',
+    artist: 'NoCopyrightSounds',
+    year: 2024,
+    url: "./Audios/NIVIRO  Without You feat Justin J Moore  Happy Hardcore  NCS  Copyright Free Music.mp3",
+    image: "./img/NCS-Niviro.png"
+})
+    
+// ---------------------- Audios Ended ----------------------
+// ----------------------------------------------------------
+// --------------------- Adding Nodes -----------------------
+
+let playListDiv = document.getElementsByClassName("playlist-song")[0]
+let index = 0;
+
+let addingToPlaylist = (index = 0, playListDiv) => {
+    for(let l of playlist) {  
+        playListDiv.innerHTML += `
+            <div class="row main-playlist">
+                <div id="noha-playlist" class="item-playlist" onClick='togglePlayList(this, "${index}")'>
+                    <div class="row-image">
+                        <img src="img/noha_logo_circle.png" alt="" />
+                    </div>
+                    <div class="row-detail">
+                        <h4>`+ l.name +`</h4>
+                        <p> ${l.item.size} Items </p>
+                    </div>
+                    <i class="fa-solid fa-angle-down"></i>
+                </div>
+
+
+                <div class="noha-drop-box">
+                    <div class="audio-box-body">
+                        
+            
+                    </div>
+                </div>
+
+            </div>
+        `
+        index++
+    }
+}
+
+addingToPlaylist(index, playListDiv)
+
+
+
+function togglePlayList(div, playlistIndex){
+    var dropBox = div.nextSibling.nextSibling
+
+    var dropBoxItemDiv = dropBox.children[0]
+    
+    let list = displayAllPlaylistItems(playlistIndex)
+
+    dropBoxItemDiv.innerHTML = list
+        
+    dropBox.classList.toggle('show-audio-list')
+}
+
+
+
+function displayAllPlaylistItems(playlistIndex) {
+
+    let list = playlist[playlistIndex].item.display()
+
+
+    let returnData = ``
+
+    let itemIndex = 0
+    let current = list.next
+    while(current){
+
+        returnData += `
+            <div class="audio" onClick="playPlaylist(${playlistIndex}, ${itemIndex})">
+                <div class="audio-detail">
+                    <div class="marquee-texts">
+                        <h4 class="marquee">${current.data.title}</h4>
+                    </div>
+                    <p>${current.data.year}</p>
+                </div>
+            </div>
+        `
+
+        current = current.next
+        itemIndex++
+    }
+
+    return returnData
+
+}
+
+
+// let artistImgDiv = document.querySelector('.artist-image-div')
+
+let currentPlayingNode = null;
+function playPlaylist(playlistIndex, itemIndex){
+
+    currentPlayingNode = playlist[playlistIndex].item.display().next
+
+    for(let i=0; i<itemIndex; i++) {
+        currentPlayingNode = currentPlayingNode.next
+    }
+    
+
+    // currentPlayingNode = item;
+    // let audioUrl = currentPlayingNode.data.url;
+    console.log("Playing:", playlistIndex, itemIndex, currentPlayingNode);
+    play()
+    
+}
+
+let audioTitle = document.querySelector('.s-title')
+let artistName = document.querySelector('.ar-name')
+let artistImg = document.querySelector('.ar-img')
+
+function play() {
+    audioTitle.innerHTML = currentPlayingNode.data.displayTitle;
+    artistName.innerHTML = currentPlayingNode.data.artist;
+    artistImg.src = currentPlayingNode.data.image
+
+    document.querySelector(".background-image").style.backgroundImage = `url(${currentPlayingNode.data.image})`
+    song.src = currentPlayingNode.data.url
+    console.log(song)
+    artistImgDiv.style.animation = ''
+    setTimeout(() => {
+        artistImgDiv.style.animation = 'artist-img 50s linear infinite'
+    }, 2000)
+}
+
+
+function playNext() {
+    if (currentPlayingNode && currentPlayingNode.next) {
+        currentPlayingNode = currentPlayingNode.next;
+        play()
+    }
+    else {
+        alert("No next audio available");
+    }
+}
+
+function playPrevious() {
+    if (currentPlayingNode && currentPlayingNode.prev.data != null) {
+        currentPlayingNode = currentPlayingNode.prev;
+        play()
+    }
+    else {
+        alert("No previous audio available");
+    }
+}
+
+
+
+// ------------------- Adding Nodes Ended -------------------
+// ----------------------------------------------------------
+// ------------------ Searching Algorithm -------------------
+
+let searchIcn = document.querySelector('.fa-magnifying-glass')
+let searchItems = document.querySelector('.search-items')
+let searchItemsBox = document.querySelector('.search-items > .audio-box-body')
+
+searchIcn.onclick = () => {
+    searchBox.style.width = '200px'
+    searchIcn.style.right = '20px'
+}
+searchBox.addEventListener('input', () => {
+    let results = new Stack()
+    playListDiv.style.display = 'none'
+    searchItems.style.display = 'grid'
+
+    for(let pIndex = 0; pIndex<playlist.length; pIndex++) {
+        let temp = playlist[pIndex].item.display()
+        let itemIndex = 0
+        while(temp.next != null) {
+            temp = temp.next
+            if (temp.data.title.toLowerCase().includes(searchBox.value.toLowerCase())) {
+                    results.push({
+                        data: temp,
+                        pIndex,
+                        itemIndex
+                    });
+            }
+
+            itemIndex++
+        }
+    }
+    
+    if(results.display().length == 0)
+        count++
+    else
+        count = 0
+    
+
+    console.log(results.display())
+
+    searchItemsBox.innerHTML = ''
+    for(let res of results.display()) {
+        searchItemsBox.innerHTML += `
+            <div class="audio" onClick="playPlaylist(${res.pIndex}, ${res.itemIndex})">
+                <div class="audio-detail">
+                    <div class="marquee-texts">
+                        <h4 class="marquee">${res.data.data.title}</h4>
+                    </div>
+                    <p>${res.data.data.year}</p>
+                </div>
+        </div>
+        `
+    }
+
+    if(searchBox.value == '') {
+        playListDiv.style.display = 'grid'
+        searchItems.style.display = 'none'
+        for(let i=0; i<results.display().length; i++) {
+            results.pop()
+        }   
+    }
+})
+
+
+// --------------- Searching Algorithm Ended ----------------
+// ----------------------------------------------------------
+// ------------------- Sorting Algorithm --------------------
+
+function sortAudioByDate() {
+    let swapped;
+    let current;
+    let end = null;
+    for(let i=0; i<3; i++) {
+        do {
+            swapped = false;
+            current = playlist[i].item.display().next;
+            while (current.next !== null) {
+                if (current.data.year > current.next.data.year) {
+                    const temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    swapped = true;
+                }
+                current = current.next;
+            }
+            end = current;
+        } while (swapped);
+    }
+}
+
+function sortAudioByName() {
+    let swapped;
+    let current;
+    let end = null;
+    for(let i=0; i<3; i++) {
+        do {
+            swapped = false;
+            current = playlist[i].item.display().next;
+            while (current.next !== null) {
+                if (current.data.title > current.next.data.title) {
+                    const temp = current.data;
+                    current.data = current.next.data;
+                    current.next.data = temp;
+                    swapped = true;
+                }
+                current = current.next;
+            }
+            end = current;
+        } while (swapped);
+    }
+}
+
+const sortDiv = document.getElementById('sort-options')
+
+sortDiv.addEventListener('change', function() {
+    if (this.value === 'date') {
+        playListDiv.innerHTML = ''
+        addingToPlaylist(index, playListDiv)
+        sortAudioByDate();
+    }
+    else if (this.value === 'name') {
+        playListDiv.innerHTML = ''
+        addingToPlaylist(index, playListDiv)
+        sortAudioByName();
+    }
+});
+
+// ------------------- Sorting Algorithm Ended ------------------------
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let artistImgDiv = document.querySelector('.artist-image-div')
 let progress = document.getElementById('progress')
 let controlIcn = document.getElementById('control-icon')
 let song = document.getElementById('song')
@@ -14,11 +15,14 @@ let playPause = () => {
             song.play()
             icon.remove('fa-play')
             icon.add('fa-pause')
+            
+            artistImgDiv.style.animation = 'artist-img 50s linear infinite'
         }
         else if(icon.contains('fa-pause')) {
             song.pause()
             icon.remove('fa-pause')
             icon.add('fa-play')
+        
         }
     } catch(err) {
         console.warn(err)
@@ -46,4 +50,29 @@ arrow.addEventListener('click', () => {
     arrow.classList.toggle('turn-arrow')
     main_player.classList.toggle('hide-l-player')
     playlist_player.classList.toggle('hide-r-player')
+})
+
+
+// ---- Playlist Tag ----
+let playlistArrow = document.querySelector('.fa-angle-right')
+let playlistTag = document.querySelector('.playlist-tag')
+playlistArrow.addEventListener('mouseover', () => {
+    setTimeout(() => {
+        playlistTag.style.display = 'flex'
+    }, 800)
+})
+
+let searchDiv = document.querySelector('.search-div')
+let searchBox = document.querySelector('.search-div > input')
+
+document.addEventListener('mousemove', (e) => {
+    if(!playlistArrow.contains(e.target))
+        playlistTag.style.display = 'none'
+})
+document.addEventListener('click', (e) => {
+    if(!searchDiv.contains(e.target)) {
+        searchIcn.style.right = '25px'
+        searchBox.style.width = 0
+        searchBox.style.paddingRight = '10px'
+    }
 })
